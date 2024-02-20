81042722c7bc6bad351e8909f25e39abbb6c34aa
Lanhui-Chen <lchen245@uottawa.ca>
2024-02-20 06:50:25 -0500

    Test

---
+++ Second-hand-book-front/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/components/Navigation/Navigation';
+
+function App() {
+  return (
+    <div className="">
+      <div>
+        <Navigation></Navigation>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
